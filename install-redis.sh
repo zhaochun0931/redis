b3986992327d97e6f8b5dde5b36a789419322869
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-10-24 18:03:01 +0800

    Update install-redis.sh

--- install-redis.sh
+++ install-redis.sh
@@ -4,3 +4,14 @@ sudo chmod 644 /usr/share/keyrings/redis-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
 sudo apt-get update
 sudo apt-get install redis
+
+
+
+
+
+
+sudo systemctl enable redis-server
+sudo systemctl start redis-server
+
+
+systemctl status redis-server
